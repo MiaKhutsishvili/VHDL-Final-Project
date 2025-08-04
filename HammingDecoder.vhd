79b2a5041b57d994f355b149c063667acb163c07
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2025-08-04 13:47:23 +0330

    Update HammingDecoder.vhd

--- HammingDecoder.vhd
+++ HammingDecoder.vhd
@@ -2,119 +2,118 @@
 -- Kimia Khoodsiyani & Maral Torabi
 -- 40223030            40223019
 
--- Create Date:    14:17:47 07/25/2025  
--- Module Name:    HammingEncoder - Behavioral 
--- Project Name:   Final Project
--- Description:    This module will receive an 8 bit serial data and will encode it using hamming method.
---
--- Dependencies:   Odd mode = 0
+-- Create Date:    11:51:06 07/31/2025 
+-- Module Name:    HammingDecoder - Behavioral 
+-- Project Name:   Logical Circuits Final Project
+-- Description:    This module will encode a 13 bit extended hamming coded data to extract 8 data bits
+
+-- Dependencies:   Odd_Mode = 1 /constant
 ----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity HammingEncoder is
-    Port ( 	clk : in STD_LOGIC;           -- Receiving sequential data
-		In_Start : in STD_LOGIC;      -- Marking In_Data LSB
-		In_Data : in  STD_LOGIC;      -- Input data
-           	Rst : in  STD_LOGIC;          -- Setting everything to the default
+entity HammingDecoder is
+    Port ( 	in_data : in  STD_LOGIC;                         	-- 1 bit input data
+		in_start : in STD_LOGIC;                         	-- is 1 when the input code starts
+		RST : in STD_LOGIC;                              	-- resets everything
+		clk : in STD_LOGIC;
+			  
+		out_rdy : inout  STD_LOGIC;                      	-- is 1 if the output is calculated
            
-		Open_In : inout  STD_LOGIC;   -- No calculation is being done
+		out_data : out  STD_LOGIC_VECTOR (7 downto 0);   	-- 8 bit output data
+           	valid_out : out  STD_LOGIC                      	-- is 1 if there is max 1 error in code
+			  );                             	
+end HammingDecoder;
 
-		Out_Data : out  STD_LOGIC;    -- Hamming coded data
-           	Out_Rdy : out  STD_LOGIC      -- Is true if the output is calculated
-			 );   
-end HammingEncoder;
+architecture Behavioral of HammingDecoder is
 
-architecture Behavioral of HammingEncoder is
-
-	signal code : STD_LOGIC_VECTOR (12 downto 0);
-
+	signal marker : STD_LOGIC_VECTOR (3 downto 0); 	         	-- possible 1 error position
+	signal extention : STD_LOGIC;                            	-- remade bit #13
+	signal inp_enc_data : STD_LOGIC_VECTOR (12 downto 0);	   	-- 13bit data
+	
 begin
-
-	process (clk, Rst, In_Start)
-		variable cnt : integer := 0;	-- cnt is 0 at the begining of simulation
-	begin
-		if rising_edge(clk) then
-			if (Rst = '1' or In_Start = '1') then
-				Out_Data <= '0';
-				Out_Rdy <= '0';
-				Open_In <= '1';
-				code <= "0000000000000";
-				cnt := 0;
-			else
-				if Open_In = '1' then	-- Input gate is open, we are in INPUT MODE
+		process (clk, RST)
+			variable cnt : integer := 0;	-- cnt is 0 at the begining of simulation
+			variable ind : integer := 0;  -- possible error position
+		begin
+			if rising_edge(clk) then
+				if (RST = '1' or in_start = '1') then
+					valid_out <= '0';
+					out_rdy <= '0';
+					out_data <= "00000000";
+					marker <= "0000";
+					inp_enc_data <= "0000000000000";
+					cnt := 0;
+					ind := 0;
+				else
+					-- taking the input
 					case cnt is
 						when 0 => 
-							code(2) <= In_Data;
+							inp_enc_data(0) <= in_data;
 						when 1 =>
-							code(4) <= In_Data;
+							inp_enc_data(1) <= in_data;
 						when 2 => 
-							code(5) <= In_Data;
+							inp_enc_data(2) <= in_data;
 						when 3 =>
-							code(6) <= In_Data;
+							inp_enc_data(3) <= in_data;
 						when 4 => 
-							code(7) <= In_Data;
+							inp_enc_data(4) <= in_data;
 						when 5 =>
-							code(8) <= In_Data;
+							inp_enc_data(5) <= in_data;
 						when 6 => 
-							code(9) <= In_Data;
+							inp_enc_data(6) <= in_data;
 						when 7 =>
-							code(10) <= In_Data;
-							cnt := 0;
-							Open_In <= '0';
-						when others =>
-							-- Impossible!
-					end case;
-					cnt := cnt + 1;
-				elsif Open_In = '0' then	-- Input gate is closed, we are in OUTPUT MODE  
-					-- Assigning parities
-					code(0) <= (((code(2) xor code(4)) xor code(6)) xor code(8)) xor code(10);
-					code(1) <= (((code(2) xor code(5)) xor code(6)) xor code(9)) xor code(10);
-					code(3) <= ((code(4) xor code(5)) xor code(6)) xor code(11);
-					code(7) <= ((code(8) xor code(9)) xor code(10)) xor code(11);
-					code(12) <= (((((code(0) xor code(1)) xor code(2)) xor code(3)) xor code(4)) xor code(5)) xor
-									(((((code(6) xor code(7)) xor code(8)) xor code(9)) xor code(10)) xor code(11));
-					-- Outputing
-					Out_Rdy <= '1';
-					case cnt is
-						when 0 => 
-							Out_Data <= code(0);
-						when 1 =>
-							Out_Data <= code(1);
-						when 2 => 
-							Out_Data <= code(2);
-						when 3 =>
-							Out_Data <= code(3);
-						when 4 => 
-							Out_Data <= code(4);
-						when 5 =>
-							Out_Data <= code(5);
-						when 6 => 
-							Out_Data <= code(6);
-						when 7 =>
-							Out_Data <= code(7);
+							inp_enc_data(7) <= in_data;
 						when 8 => 
-							Out_Data <= code(8);
+							inp_enc_data(8) <= in_data;
 						when 9 =>
-							Out_Data <= code(9);
+							inp_enc_data(9) <= in_data;
 						when 10 => 
-							Out_Data <= code(10);
+							inp_enc_data(10) <= in_data;
 						when 11 =>
-							Out_Data <= code(11);
+							inp_enc_data(11) <= in_data;
 						when 12 => 
-							Out_Data <= code(12);
-							cnt := 0;
-							Open_In <= '1';
-							Out_Rdy <= '0';
+							inp_enc_data(12) <= in_data;
 						when others =>
 							-- Impossible!
 					end case;
 					cnt := cnt + 1;
+					
+					if cnt = 13 then
+						cnt := 0;
+						-- Constructing the new parities
+						marker(0) <= inp_enc_data(0) xor (inp_enc_data(2) xor (inp_enc_data(4) xor
+										(inp_enc_data(6) xor (inp_enc_data(8) xor inp_enc_data(10)))));
+						marker(1) <= inp_enc_data(1) xor (inp_enc_data(2) xor (inp_enc_data(5) xor
+										(inp_enc_data(6) xor (inp_enc_data(9) xor inp_enc_data(10)))));
+						marker(2) <= inp_enc_data(3) xor (inp_enc_data(4) xor (inp_enc_data(5) xor
+										(inp_enc_data(6) xor inp_enc_data(11))));
+						marker(3) <= inp_enc_data(7) xor (inp_enc_data(8) xor (inp_enc_data(9) xor
+										(inp_enc_data(10) xor inp_enc_data(11))));
+						extention <= inp_enc_data(0) xor (inp_enc_data(1) xor (inp_enc_data(2) xor
+										(inp_enc_data(3) xor (inp_enc_data(4) xor (inp_enc_data(5) xor
+										(inp_enc_data(6) xor (inp_enc_data(7) xor (inp_enc_data(8) xor
+										(inp_enc_data(9) xor (inp_enc_data(10) xor inp_enc_data(11)))))))))));
+						-- Correction
+						ind :=  to_integer(unsigned(marker));
+						ind := ind - 1;
+						if ((ind = -1 and extention = inp_enc_data(12)) or
+							  ((ind > -1 and ind < 12) and (extention = not inp_enc_data(12)))) then
+							valid_out <= '1';
+							if ind > -1 then
+								inp_enc_data(ind) <= not inp_enc_data(ind);
+							end if;
+						else
+							valid_out <= '0';
+						end if;
+						out_rdy <= '1';
+						out_data <= inp_enc_data(11) & inp_enc_data(10) & inp_enc_data(9) & inp_enc_data(8) &
+										inp_enc_data(6) & inp_enc_data(5) & inp_enc_data(4) & inp_enc_data(2);
+					end if;
 				end if;
 			end if;
-		end if;	
-	end process;
-
+		end process;
 end Behavioral;
 
