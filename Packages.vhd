294dfa9d43066882cd12ec06bc5ac768b907b4c2
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2025-08-04 05:13:41 +0330

    Update Packages.vhd

--- Packages.vhd
+++ Packages.vhd
@@ -9,14 +9,11 @@ use IEEE.STD_LOGIC_1164.all;
 package Packages is
 
 	type data_packet is array (0 to 6) of STD_LOGIC_VECTOR(7 downto 0);
---	type Operand_Alu is array (0 to 5) of STD_LOGIC_VECTOR(7 downto 0);			
---	type Writ_e is array (0 to 4) of STD_LOGIC_VECTOR(7 downto 0);		  		
---	type Rea_d is array (0 to 3) of STD_LOGIC_VECTOR(7 downto 0);				
---	type Immediate_Alu is array (0 to 5) of STD_LOGIC_VECTOR(7 downto 0);		
---	type Array_Alu is array (0 to 6) of STD_LOGIC_VECTOR(7 downto 0);			
---	type Indirect_Addressing is array (0 to 5) of STD_LOGIC_VECTOR(7 downto 0);	
-
 	type pack_type is (Operand_Alu, Writ_e, Rea_d, Immediate_Alu, Array_Alu, Indirect_Addressing, zero, Read_Response);
+	
+	type Ram_Row is array (0 to 7) of STD_LOGIC_VECTOR(7 downto 0);
+	type Ram_Matrix is array (0 to 31) of Ram_Row;
+	type Ram_Pack is array (0 to 4) of STD_LOGIC_VECTOR(7 downto 0);
 	
 end Packages;
 
