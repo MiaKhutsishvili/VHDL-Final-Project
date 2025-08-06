b5997e1370d79799efdcf0ebb3a3c9d1ef4a990f
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2025-08-06 16:38:32 +0330

    Update ControlUnit.vhd

--- ControlUnit.vhd
+++ ControlUnit.vhd
@@ -13,65 +13,36 @@ use IEEE.NUMERIC_STD.ALL;
 use work.Packages.ALL;
 
 entity ControlUnit is
-    Port ( 	in_data : in  STD_LOGIC_VECTOR(7 downto 0);			-- 8 bit input data
-		-- in_start : in STD_LOGIC;									
-		clk : in STD_LOGIC;				
-		RST : in STD_LOGIC;						-- Resets everything
-			  
-		out_rdy : inout STD_LOGIC;					-- Is 1 when the whole packet is ready 
-		packet_type : inout pack_type;					-- Determines the type packet
+    Port ( in_data : in Byte;								-- 8 bit input data
+			  packet_type : inout pack_type;				-- Determines the type packet
 
-		out_data : out data_packet					-- Whole packet output
+			  out_data : out data_packet;					-- Whole packet output
+
+			  clk : in STD_LOGIC;				
+			  RST : in STD_LOGIC								-- Resets everything
 			);
 end ControlUnit;
 
 architecture Behavioral of ControlUnit is
 	
-	signal packet : data_packet;						-- Temperory memory for a packet
---	signal waitOneClk : STD_LOGIC;						-- Used to make a 1 clk hold for the out_rdy
-	signal step : integer := 0;						-- To save which part of packet is the input
-	signal intSum : integer range -650 to 650 := 0;				-- From -2^7 * 5 to (2^7 - 1) * 5
+	signal packet : data_packet := (others => (others => '0'));		-- Temperory memory for a packet
+	signal step : integer := 0;												-- To save which part of packet is the input
+	signal intSum : integer range -650 to 650 := 0;						-- From -2^7 * 5 to (2^7 - 1) * 5
 	signal bitSum : STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000";
-	signal local_rst : STD_LOGIC := '0';
 	
 begin
 
 	process (clk)
 	begin
 		if rising_edge(clk) then
---			if in_start = '1' then
---				for i in 0 to 6 loop
---					for j in 0 to 7 loop
---						packet(i)(j) <= '0';
---					end loop;
---				end loop;
---				step <= 0;
---				waitOneClk <= '0';
---			end if;
-			if RST = '1' or local_rst = '1' then
-				for i in 0 to 6 loop
-					for j in 0 to 7 loop
-						packet(i)(j) <= '0';
-					end loop;
-				end loop;
+			if RST = '1' then
+				packet <= (others => (others => '0'));
 				step <= 0;
---				waitOneClk <= '0';
-				intSum <= 0;
-				bitSum <= "0000000000000000";
 			else
---			if waitOneClk = '1' then 
---				out_rdy <= '0';
---				waitOneClk <= '0';
---			end if;
---			
---			if out_rdy = '1' then
---				waitOneClk <= '1';
---			end if; 
-			
 				case step is
 					when 0 =>
 						packet(0) <= in_data;
-						case packet(0) is 																-- Categorizing the packet
+					case packet(0) is 																-- Categorizing the packet
 							when "00000000" | "00000001" | "00000010" | "00000011" =>
 								packet_type <= Operand_Alu;
 							when "11110000" => 
@@ -88,72 +59,54 @@ begin
 								packet_type <= Read_Response;
 							when others =>
 								packet_type <= zero;
-						end case;
+						end case;	
 						
 					when 1 =>
 						packet(1) <= in_data;
-						if (packet_type = Rea_d or packet_type = Read_Response) then
-							-- sum 
-							intSum <= to_integer(signed(packet(0))) + to_integer(signed(packet(1)));
-							bitSum <= std_logic_vector(to_signed(intSum, 16));
-							packet(2) <= bitSum(15 downto 8);
-							packet(3) <= bitSum(7 downto 0);
-							packet(4) <= "00000000";
-							packet(5) <= "00000000";
-							packet(6) <= "00000000";
-							out_rdy <= '1';
-							out_data <= packet;
-							local_rst <= '1';
-						end if;
 					
 					when 2 => 
-						packet(2) <= in_data;
-						if packet_type = Writ_e then
-							intSum <= to_integer(signed(packet(0))) + to_integer(signed(packet(1))) + to_integer(signed(packet(2)));
-							bitSum <= std_logic_vector(to_signed(intSum, 16));
-							packet(3) <= bitSum(15 downto 8);
-							packet(4) <= bitSum(7 downto 0);
-							packet(5) <= "00000000";
-							packet(6) <= "00000000";
-							out_rdy <= '1';
-							out_data <= packet;
-							local_rst <= '1';
+						if packet_type = Rea_d then
+							packet(2) <= "00000000";
+							step <= 3;
+						else
+							packet(2) <= in_data;
 						end if;
-						
+
 					when 3 => 
-						packet(3) <= in_data;
-						if ((packet_type = Operand_Alu or packet_type = Immediate_Alu) or packet_type = Indirect_Addressing) then
-							intSum <= to_integer(signed(packet(0))) + to_integer(signed(packet(1))) +
-										 to_integer(signed(packet(2))) + to_integer(signed(packet(3)));
-							bitSum <= std_logic_vector(to_signed(intSum, 16));
-							packet(4) <= bitSum(15 downto 8);
-							packet(5) <= bitSum(7 downto 0);
-							packet(6) <= "00000000";
-							out_rdy <= '1';
-							out_data <= packet;
-							local_rst <= '1';
+						if (packet_type = Rea_d or packet_type = Writ_e) then
+							packet(3) <= "00000000";
+							step <= 4;
+						else
+							packet(3) <= in_data;
 						end if;
 						
 					when 4 => 
-						packet(4) <= in_data;
 						if packet_type = Array_Alu then
-							intSum <= to_integer(signed(packet(0))) + to_integer(signed(packet(1))) +
-										 to_integer(signed(packet(2))) + to_integer(signed(packet(3))) +
-										 to_integer(signed(packet(4)));
-							bitSum <= std_logic_vector(to_signed(intSum, 16));
-							packet(5) <= bitSum(15 downto 8);
-							packet(6) <= bitSum(7 downto 0);
-							out_rdy <= '1';
-							out_data <= packet;
-							local_rst <= '1';
+							packet(4) <= in_data;
+						else
+							packet(4) <= "00000000";
 						end if;
+						step <= 5;
+					
+					when 5 => 
+						intSum <= to_integer(signed(packet(0))) + to_integer(signed(packet(1))) +
+									 to_integer(signed(packet(2))) + to_integer(signed(packet(3))) +
+									 to_integer(signed(packet(4)));
+						bitSum <= std_logic_vector(to_signed(intSum, 16));
+						packet(5) <= bitSum(15 downto 8);
+						step <= 6;
+					
+					when 6 =>
+						packet(6) <= bitSum(7 downto 0);
+						step <= 7;
 						
 					when others =>
-						-- Oopsi;
+						out_data <= packet;
+						step <= -1;
 				end case;
 				step <= step + 1;
+				
 			end if;
-			local_rst <= '0';
 		end if;
 	end process;
 	
