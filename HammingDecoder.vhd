7c92c7c4caf3b78570562786127109957b6fcfeb
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2025-08-06 16:38:56 +0330

    Update HammingDecoder.vhd

--- HammingDecoder.vhd
+++ HammingDecoder.vhd
@@ -16,15 +16,15 @@ use IEEE.NUMERIC_STD.ALL;
 use work.Packages.ALL;
 
 entity HammingDecoder is
-    Port ( 	in_data : in  STD_LOGIC;                        -- 1 bit input data
-		in_start : in STD_LOGIC;                        -- is 1 when the input code starts
-		RST : in STD_LOGIC;                             -- resets everything
-		clk : in STD_LOGIC;
+    Port ( in_data : in STD_LOGIC;                         		-- 1 bit input data
+			  --in_start : in STD_LOGIC;                         	-- is 1 when the input code starts
+			  RST : in STD_LOGIC;                              	-- resets everything
+			  clk : in STD_LOGIC;
 			  
-		out_rdy : inout  STD_LOGIC;                     -- is 1 if the output is calculated
+			  --out_rdy : inout  STD_LOGIC;                      	-- is 1 if the output is calculated
            
-		out_data : out Byte;   				-- 8 bit output data
-           	valid_out : out STD_LOGIC                      	-- is 1 if there is max 1 error in code
+			  out_data : out Byte;   										-- 8 bit output data
+           valid_out : out STD_LOGIC                      		-- is 1 if there is max 1 error in code
 			  );                             	
 end HammingDecoder;
 
@@ -35,54 +35,24 @@ architecture Behavioral of HammingDecoder is
 	signal inp_enc_data : STD_LOGIC_VECTOR (12 downto 0);	   	-- 13bit data
 	
 begin
-		process (clk, RST)
+		process (clk)
 			variable cnt : integer := 0;	-- cnt is 0 at the begining of simulation
-			variable ind : integer := 0;  	-- possible error position
+			variable ind : integer := 0;  -- possible error position
 		begin
 			if rising_edge(clk) then
-				if (RST = '1' or in_start = '1') then
+				if (RST = '1') then --or in_start = '1') then
 					valid_out <= '0';
-					out_rdy <= '0';
-					out_data <= "00000000";
+					--out_rdy <= '0';
 					marker <= "0000";
 					inp_enc_data <= "0000000000000";
 					cnt := 0;
 					ind := 0;
 				else
 					-- taking the input
-					case cnt is
-						when 0 => 
-							inp_enc_data(0) <= in_data;
-						when 1 =>
-							inp_enc_data(1) <= in_data;
-						when 2 => 
-							inp_enc_data(2) <= in_data;
-						when 3 =>
-							inp_enc_data(3) <= in_data;
-						when 4 => 
-							inp_enc_data(4) <= in_data;
-						when 5 =>
-							inp_enc_data(5) <= in_data;
-						when 6 => 
-							inp_enc_data(6) <= in_data;
-						when 7 =>
-							inp_enc_data(7) <= in_data;
-						when 8 => 
-							inp_enc_data(8) <= in_data;
-						when 9 =>
-							inp_enc_data(9) <= in_data;
-						when 10 => 
-							inp_enc_data(10) <= in_data;
-						when 11 =>
-							inp_enc_data(11) <= in_data;
-						when 12 => 
-							inp_enc_data(12) <= in_data;
-						when others =>
-							-- Impossible!
-					end case;
-					cnt := cnt + 1;
-					
-					if cnt = 13 then
+					if cnt < 13 then
+						inp_enc_data <= in_data & inp_enc_data(12 downto 1);
+						cnt := cnt + 1;
+					else
 						cnt := 0;
 						-- Constructing the new parities
 						marker(0) <= inp_enc_data(0) xor (inp_enc_data(2) xor (inp_enc_data(4) xor
@@ -109,7 +79,7 @@ begin
 						else
 							valid_out <= '0';
 						end if;
-						out_rdy <= '1';
+						--out_rdy <= '1';
 						out_data <= inp_enc_data(11) & inp_enc_data(10) & inp_enc_data(9) & inp_enc_data(8) &
 										inp_enc_data(6) & inp_enc_data(5) & inp_enc_data(4) & inp_enc_data(2);
 					end if;
