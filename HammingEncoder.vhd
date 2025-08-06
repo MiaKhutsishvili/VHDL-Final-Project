ddf8b0eb6917cdfe3e31dc4da6f9919dbe87bcff
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2025-08-06 16:39:15 +0330

    Update HammingEncoder.vhd

--- HammingEncoder.vhd
+++ HammingEncoder.vhd
@@ -12,105 +12,89 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use work.Packages.All;
 
 entity HammingEncoder is
-    Port ( 	clk : in STD_LOGIC;           	-- Receiving sequential data
-		In_Start : in STD_LOGIC;      	-- Marking In_Data LSB
-		In_Data : in  STD_LOGIC;      	-- Input data
-           	Rst : in  STD_LOGIC;          	-- Setting everything to the default
+    Port ( clk : in STD_LOGIC;           -- Receiving sequential data
+			  in_data : in  STD_LOGIC;      -- Input data
+           RST : in  STD_LOGIC;          -- Setting everything to the default
            
-		Open_In : inout  STD_LOGIC;	-- No calculation is being done
-
-		Out_Data : out  STD_LOGIC;    	-- Hamming coded data
-        	Out_Rdy : out  STD_LOGIC      	-- Is true if the output is calculated
+			  out_data : out  STD_LOGIC    -- Hamming coded data
 			 );    
 end HammingEncoder;
 
 architecture Behavioral of HammingEncoder is
 
-	signal code : STD_LOGIC_VECTOR (12 downto 0);
+	signal code : BYTE := "00000000";
+	signal enc_data : STD_LOGIC_VECTOR(12 downto 0) := "0000000000000";
 
 begin
 
-	process (clk, Rst, In_Start)
-		variable cnt : integer := 0;	-- cnt is 0 at the begining of simulation
+	process (clk)
+		variable in_cnt : integer := 0;	-- cnt is 0 at the begining of simulation
+		variable out_cnt : integer := 0;
 	begin
 		if rising_edge(clk) then
-			if (Rst = '1' or In_Start = '1') then
-				Out_Data <= '0';
-				Out_Rdy <= '0';
-				Open_In <= '1';
-				code <= "0000000000000";
-				cnt := 0;
+			if (RST = '1') then 
+				out_data <= '0';
+				code <= "00000000";
+				enc_data <= "0000000000000";
+				in_cnt := 0;
+				out_cnt := 0;
 			else
-				if Open_In = '1' then	-- Input gate is open, we are in INPUT MODE
-					case cnt is
-						when 0 => 
-							code(2) <= In_Data;
-						when 1 =>
-							code(4) <= In_Data;
-						when 2 => 
-							code(5) <= In_Data;
-						when 3 =>
-							code(6) <= In_Data;
-						when 4 => 
-							code(7) <= In_Data;
-						when 5 =>
-							code(8) <= In_Data;
-						when 6 => 
-							code(9) <= In_Data;
-						when 7 =>
-							code(10) <= In_Data;
-							cnt := 0;
-							Open_In <= '0';
-						when others =>
-							-- Impossible!
-					end case;
-					cnt := cnt + 1;
-				elsif Open_In = '0' then	-- Input gate is closed, we are in OUTPUT MODE  
+				if in_cnt < 8 then	-- Input gate is open, we are in INPUT MODE
+					code <= in_data & code(7 downto 1);
+					in_cnt := in_cnt + 1;
+				else
+					in_cnt := 0;
+					enc_data(2) <= code(0);
+					enc_data(4) <= code(1);
+					enc_data(5) <= code(2);
+					enc_data(6) <= code(3);
+					enc_data(8) <= code(4);
+					enc_data(9) <= code(5);
+					enc_data(10) <= code(6);
+					enc_data(12) <= code(7);
 					-- Assigning parities
-					code(0) <= (((code(2) xor code(4)) xor code(6)) xor code(8)) xor code(10);
-					code(1) <= (((code(2) xor code(5)) xor code(6)) xor code(9)) xor code(10);
-					code(3) <= ((code(4) xor code(5)) xor code(6)) xor code(11);
-					code(7) <= ((code(8) xor code(9)) xor code(10)) xor code(11);
-					code(12) <= (((((code(0) xor code(1)) xor code(2)) xor code(3)) xor code(4)) xor code(5)) xor
-									(((((code(6) xor code(7)) xor code(8)) xor code(9)) xor code(10)) xor code(11));
+					enc_data(0) <= (((enc_data(2) xor enc_data(4)) xor enc_data(6)) xor enc_data(8)) xor enc_data(10);
+					enc_data(1) <= (((enc_data(2) xor enc_data(5)) xor enc_data(6)) xor enc_data(9)) xor enc_data(10);
+					enc_data(3) <= ((enc_data(4) xor enc_data(5)) xor enc_data(6)) xor enc_data(11);
+					enc_data(7) <= ((enc_data(8) xor enc_data(9)) xor enc_data(10)) xor enc_data(11);
+					enc_data(12) <= (((((enc_data(0) xor enc_data(1)) xor enc_data(2)) xor enc_data(3)) xor enc_data(4)) xor enc_data(5)) xor
+									(((((enc_data(6) xor enc_data(7)) xor enc_data(8)) xor enc_data(9)) xor enc_data(10)) xor enc_data(11));
 					-- Outputing
-					Out_Rdy <= '1';
-					case cnt is
+					--Out_Rdy <= '1';
+					case out_cnt is
 						when 0 => 
-							Out_Data <= code(0);
+							out_data <= enc_data(0);
 						when 1 =>
-							Out_Data <= code(1);
+							out_data <= enc_data(1);
 						when 2 => 
-							Out_Data <= code(2);
+							out_data <= enc_data(2);
 						when 3 =>
-							Out_Data <= code(3);
+							out_data <= enc_data(3);
 						when 4 => 
-							Out_Data <= code(4);
+							out_data <= enc_data(4);
 						when 5 =>
-							Out_Data <= code(5);
+							out_data <= enc_data(5);
 						when 6 => 
-							Out_Data <= code(6);
+							out_data <= enc_data(6);
 						when 7 =>
-							Out_Data <= code(7);
+							out_data <= enc_data(7);
 						when 8 => 
-							Out_Data <= code(8);
+							out_data <= enc_data(8);
 						when 9 =>
-							Out_Data <= code(9);
+							out_data <= enc_data(9);
 						when 10 => 
-							Out_Data <= code(10);
+							out_data <= enc_data(10);
 						when 11 =>
-							Out_Data <= code(11);
+							out_data <= enc_data(11);
 						when 12 => 
-							Out_Data <= code(12);
-							cnt := 0;
-							Open_In <= '1';
-							Out_Rdy <= '0';
+							out_data <= enc_data(12);
+							out_cnt := -1;
 						when others =>
-							-- Impossible!
 					end case;
-					cnt := cnt + 1;
+					out_cnt := out_cnt + 1;
 				end if;
 			end if;
 		end if;	
