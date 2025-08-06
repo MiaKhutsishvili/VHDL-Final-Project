7154853b2ad0414cd64cfa7191112977bc97ca76
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2025-08-06 10:47:49 +0330

    Update Packages.vhd

--- Packages.vhd
+++ Packages.vhd
@@ -10,8 +10,8 @@ package Packages is
 	type data_packet is array (0 to 6) of STD_LOGIC_VECTOR(7 downto 0);
 	type pack_type is (Operand_Alu, Writ_e, Rea_d, Immediate_Alu, Array_Alu, Indirect_Addressing, zero, Read_Response);
 	
-	type Ram_Row is array (0 to 7) of STD_LOGIC_VECTOR(7 downto 0);
-	type Ram_Matrix is array (0 to 31) of Ram_Row;
+	--type Ram_Row is array (0 to 7) of STD_LOGIC_VECTOR(7 downto 0);
+	type Ram_Matrix is array (0 to 31) of STD_LOGIC_VECTOR(7 downto 0); --Ram_Row;
 	type Ram_In_Pack is array (0 to 4) of STD_LOGIC_VECTOR(7 downto 0);
 	type Ram_Resp_Pack is array (0 to 3) of STD_LOGIC_VECTOR(7 downto 0);
 	
