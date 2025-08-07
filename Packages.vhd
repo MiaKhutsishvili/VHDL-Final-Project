fe7cda1c137f2b9bd2621d3b25979dec65598d99
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2025-08-07 22:51:02 +0330

    Update Packages.vhd

--- Packages.vhd
+++ Packages.vhd
@@ -11,55 +11,21 @@ package Packages is
 	subtype byte is STD_LOGIC_VECTOR(7 downto 0);
 
 	type data_packet is array (0 to 6) of Byte;
-	type main_switch is (Ram, Alu);
-	type packet_type is (Operand_Alu, Writ_e, Rea_d, Immediate_Alu, Array_Alu, Indirect_Addressing, zero, Read_Response);
+	type packet_type is (Operand_Alu, Writ_e, Rea_d, Immediate_Alu, Array_Alu, Indirect_Addressing, zero);
 	type alu_operation is (Add, Sub, Orr, Andd);
 	
 	--type ram_row is array (0 to 7) of STD_LOGIC_VECTOR(7 downto 0);
 	type ram_matrix is array (0 to 31) of byte; --Ram_Row;
 	type ram_resp_pack is array (0 to 3) of Byte;
 	
-	function Validate (Packet : data_packet) return STD_LOGIC;
-	--function RRRValidate(Packet : ram_resp_pack) return STD_LOGIC;
 	function CheckSumH (Packet : data_packet) return byte;
 	function CheckSumL (Packet : data_packet) return byte;
+	function Validate (Packet : data_packet) return STD_LOGIC;
 	
 end Packages;
 
 package body Packages is
-	
-	function Validate(Packet : data_packet) return STD_LOGIC is
-		variable Sum : signed(15 downto 0) := (others => '0');
-		variable CheckH : byte;
-		variable CheckL : byte;
-	begin
-		for i in 0 to 4 loop
-			Sum := Sum + signed(Packet(i));
-		end loop;
-		CheckH := STD_LOGIC_VECTOR(Sum(15 downto 8));
-		CheckL := STD_LOGIC_VECTOR(Sum(7 downto 0));
-		if (CheckH = packet(5) and CheckL = Packet(6)) then
-			return '1';
-		else 
-			return '0';
-		end if;
-	end function;
-	
---	function RRRValidate(Packet : ram_resp_pack) return STD_LOGIC is
---		variable Sum : signed(15 downto 0) := (others => '0');
---		variable CheckH : byte;
---		variable CheckL : byte;
---	begin
---		Sum := signed(Packet(0)) + signed(Packet(1));
---		CheckH := STD_LOGIC_VECTOR(Sum(15 downto 8));
---		CheckL := STD_LOGIC_VECTOR(Sum(7 downto 0));
---		if (CheckH = packet(2) and CheckL = Packet(3)) then
---			return '1';
---		else 
---			return '0';
---		end if;
---	end function;
-	
+
 	function CheckSumH (Packet : data_packet) return byte is 
 			variable Sum : signed(15 downto 0) := (others => '0');
 	begin
@@ -77,5 +43,18 @@ package body Packages is
 		end loop;
 		return STD_LOGIC_VECTOR(Sum(7 downto 0));
 	end function;
+	
+	function Validate(Packet : data_packet) return STD_LOGIC is
+		variable CheckH, CheckL : byte;
+	begin
+		CheckH := CheckSumH(Packet);
+		CheckL := CheckSumL(Packet);
+		if (CheckH = Packet(5) and CheckL = Packet(6)) then
+			return '1';
+		else 
+			return '0';
+		end if;
+	end function;
+	
 	
 end Packages;
