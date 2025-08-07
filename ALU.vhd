be391c7aeb7bc4a1c1bcbb50263fec12a368b405
Kimia <152694792+MiaKhutsishvili@users.noreply.github.com>
2025-08-07 21:35:25 +0330

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -16,12 +16,12 @@ entity ALU is
     Port ( InPack : in  data_packet;
 			  PackMode : in  packet_type;	
 			  
-			  SendToRamPack: inout data_packet;
+			  SentToRam : out data_packet;
 			  ReadResponse : in ram_resp_pack;
 			  Finish : inout STD_LOGIC; 
 			  
+			  Enable : in STD_LOGIC;
 			  Error : out STD_LOGIC;
-			  --RST : in STD_LOGIC;
 			  clk : in STD_LOGIC
 			 );
 end ALU;
@@ -45,7 +45,8 @@ architecture Behavioral of ALU is
 	end function;
 	
 	signal operation : Alu_Operation;
-	
+	signal SendToRamPack: data_packet;
+	signal mode : byte;
 	signal DataI : byte := (others => '0');
 	signal DataII : byte := (others => '0');
 	signal AddressI : byte;
@@ -55,9 +56,9 @@ architecture Behavioral of ALU is
 	signal ArrayLength : integer range 0 to 31 := 0;
 	signal ArrayOdd : STD_LOGIC;
 	
-	signal Output : byte;
+	signal RamAdd : byte;
+	signal Output : byte := (others => '0');
 	
---	signal PackError : STD_LOGIC;
 --	signal RamRespError : STD_LOGIC;
 	
 	signal Step : integer := 0;
@@ -69,127 +70,114 @@ begin
 		variable CheckL : byte;
 	begin
 		if rising_edge(clk) then
-			if (Step = 0 or Finish = '1') then
-				Step <= 0;
-				Finish <= '0';
+			if Enable = '1' then
+				if (Step = 0 or Finish = '1') then
+					SentToRam <= SendToRamPack;
+					Step <= 0;
+					Finish <= '0';
+						
+					Error <= not(Validate(InPack));
 					
-				Error <= not(Validate(InPack));
-				
-				case InPack(0)(1 downto 0) is 
-					when "00" =>
-						operation <= Add;
-					when "01" => 
-						operation <= Sub;
-					when "10" => 
-						operation <= Orr;
-					when "11" =>
-						operation <= Andd;
-					when others =>
-				end case;			
-					
-				AddressI <= InPack(1);
-			
-				SendToRamPack <= ("00001111", AddressI, "00000000", "00000000", "00000000", "00000000", "00000000");
-				SendToRamPack(5) <= CheckSumH(SendToRamPack);
-				SendToRamPack(6) <= CheckSumL(SendToRamPack);
+					case InPack(0)(1 downto 0) is 
+						when "00" =>
+							operation <= Add;
+						when "01" => 
+							operation <= Sub;
+						when "10" => 
+							operation <= Orr;
+						when "11" =>
+							operation <= Andd;
+						when others =>
+					end case;			
+						
+					AddressI <= InPack(1);
+					RamAdd <= AddressI;
+					mode <= "00001111";
+				end if;
 				
 				case PackMode is 
 					when Operand_Alu => 
+						if Step = 0 then
 						AddressII <= InPack(2);
 						DestinationAddress <= InPack(3);
-						
-					when Immediate_Alu =>
-						DataII <= InPack(2);
-						DestinationAddress <= InPack(3);
-						
-					when Array_Alu =>
-						DataII <= InPack(2);
-						ArrayLength <= to_integer(unsigned(InPack(3)));
-						ArrayOdd <= '0';
-						DestinationAddress <= InPack(4);
-						
-					when Indirect_Addressing =>
-						AddAddressII <= InPack(2);
-						DestinationAddress <= InPack(3);
-						
-					when others =>
-				end case;
-				Step <= Step + 1;
-			
-			else
-				--RamRespError <= not(RRRValidate(ReadResponse));
-				case PackMode is 
-					when Operand_Alu => 
-						if Step = 1 then
+						elsif Step = 1 then
 							DataI <= ReadResponse(1);
-							SendToRamPack <= ("00001111", AddressII, "00000000", "00000000", "00000000", "00000000", "00000000");
-							SendToRamPack(5) <= CheckSumH(SendToRamPack);
-							SendToRamPack(6) <= CheckSumL(SendToRamPack);
+							RamAdd <= AddressII;
+							mode <= "00001111";
 						elsif Step = 2 then
 							Output <= Operator(signed(DataI), signed(DataII), Operation);
-							SendToRamPack <= ("11110000", DestinationAddress, Output, "00000000", "00000000", "00000000", "00000000");
-							SendToRamPack(5) <= CheckSumH(SendToRamPack);
-							SendToRamPack(6) <= CheckSumL(SendToRamPack);
+							RamAdd <= DestinationAddress;
+							mode <= "11110000";	
 							Finish <= '1';
 						end if;
-						
+							
 					when Immediate_Alu =>
-						DataI <= ReadResponse(1);
-						Output <= Operator(signed(DataI), signed(DataII), Operation);
-						SendToRamPack <= ("11110000", DestinationAddress, Output, "00000000", "00000000", "00000000", "00000000");
-						SendToRamPack(5) <= CheckSumH(SendToRamPack);
-						SendToRamPack(6) <= CheckSumL(SendToRamPack);
-						Finish <= '1';
-						
+						if Step = 0 then
+							DataII <= InPack(2);
+							DestinationAddress <= InPack(3);
+						elsif Step = 1 then
+							DataI <= ReadResponse(1);
+							Output <= Operator(signed(DataI), signed(DataII), Operation);
+							RamAdd <= DestinationAddress;
+							mode <= "11110000";
+							Finish <= '1';
+						end if;
+							
 					when Array_Alu =>
-						if Step < (ArrayLength + 1) then 
+						if Step = 0 then
+							DataII <= InPack(2);
+							ArrayLength <= to_integer(unsigned(InPack(3)));
+							ArrayOdd <= '0';
+							DestinationAddress <= InPack(4);
+						elsif Step < (ArrayLength + 1) then 
 							if ArrayOdd = '0' then
+								mode <= "11110000";
 								DataI <= ReadResponse(1);
 								Output <= Operator(signed(DataI), signed(DataII), Operation);
 								if to_integer(unsigned(DestinationAddress)) + Step - 1 > 31 then
 									DestinationAddress <= "00000000";
 								end if;	
-								SendToRamPack <= ("11110000", byte(unsigned(DestinationAddress) + to_unsigned(Step - 1, 8)), 
-														Output, "00000000", "00000000", "00000000", "00000000");
-								SendToRamPack(5) <= CheckSumH(SendToRamPack);
-								SendToRamPack(6) <= CheckSumL(SendToRamPack);
+								RamAdd <= byte(unsigned(DestinationAddress) + to_unsigned(Step - 1, 8));	
 							else
+								mode <= "00001111";
 								if to_integer(unsigned(AddressI)) + Step - 1 > 31 then
 									AddressI <= "00000000";
 								end if;	
-								SendToRamPack <= ("00001111", byte(unsigned(AddressI) + to_unsigned(Step - 1, 8)), 
-														"00000000", "00000000", "00000000", "00000000", "00000000");
-								SendToRamPack(5) <= CheckSumH(SendToRamPack);
-								SendToRamPack(6) <= CheckSumL(SendToRamPack);
+								RamAdd <= byte(unsigned(AddressI) + to_unsigned(Step - 1, 8));
 							end if;
 							ArrayOdd <= not(ArrayOdd);
 						else
 							Finish <= '1';
 						end if;
-						
+							
 					when Indirect_Addressing =>
-						if Step = 1 then
+						if Step = 0 then
+							AddAddressII <= InPack(2);
+							DestinationAddress <= InPack(3);
+						elsif Step = 1 then
 							DataI <= ReadResponse(1);
-							SendToRamPack <= ("00001111", AddAddressII, "00000000", "00000000", "00000000", "00000000", "00000000");
-							SendToRamPack(5) <= CheckSumH(SendToRamPack);
-							SendToRamPack(6) <= CheckSumL(SendToRamPack);
+							mode <= "00001111";
+							RamAdd <= AddAddressII;
 						elsif Step = 2 then
 							AddressII <= ReadResponse(1);
-							SendToRamPack <= ("00001111", AddressII, "00000000", "00000000", "00000000", "00000000", "00000000");
-							SendToRamPack(5) <= CheckSumH(SendToRamPack);
-							SendToRamPack(6) <= CheckSumL(SendToRamPack);
+							RamAdd <= AddressII;
+							mode <= "00001111";	
 						elsif Step = 3 then
 							DataII <= ReadResponse(1);
 							Output <= Operator(signed(DataI), signed(DataII), Operation);
-							SendToRamPack <= ("11110000", DestinationAddress, Output, "00000000", "00000000", "00000000", "00000000");
-							SendToRamPack(5) <= CheckSumH(SendToRamPack);
-							SendToRamPack(6) <= CheckSumL(SendToRamPack);
+							RamAdd <= DestinationAddress;
+							mode <= "11110000";
 							Finish <= '1';
 						end if;
-						
 					when others =>
+						Error <= '1';
 				end case;
 				Step <= Step + 1;
+				SendToRamPack <= (mode, RamAdd, Output, "00000000", "00000000", "00000000", "00000000");
+				SendToRamPack(5) <= CheckSumH(SendToRamPack);
+				SendToRamPack(6) <= CheckSumL(SendToRamPack);
+			else
+				Error <= '1';
 			end if;
 		end if;
 	end process;
